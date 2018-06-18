82663feb2f4e66d53fc2bcab39ebf317f79859ab
Brock Allen <brockallen@gmail.com>
2018-06-18 14:36:45 -0400

    test reverting expots back to pre-1.4.x style

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-export { Log } from './src/Log';
-export { OidcClient } from './src/OidcClient';
-export { OidcClientSettings } from './src/OidcClientSettings';
-export { WebStorageStateStore } from './src/WebStorageStateStore';
-export { InMemoryWebStorage } from './src/InMemoryWebStorage';
-export { UserManager } from './src/UserManager';
-export { AccessTokenEvents } from './src/AccessTokenEvents';
-export { MetadataService } from './src/MetadataService';
-export { CordovaPopupNavigator } from './src/CordovaPopupNavigator';
-export { CordovaIFrameNavigator } from './src/CordovaIFrameNavigator';
-export { CheckSessionIFrame } from './src/CheckSessionIFrame';
-export { TokenRevocationClient } from './src/TokenRevocationClient';
-export { SessionMonitor } from './src/SessionMonitor';
-export { Global } from './src/Global';
-export { User } from './src/User';
+let Log = require('./src/Log');
+let OidcClient = require('./src/OidcClient');
+let OidcClientSettings = require('./src/OidcClientSettings');
+let WebStorageStateStore = require('./src/WebStorageStateStore');
+let InMemoryWebStorage = require('./src/InMemoryWebStorage');
+let UserManager = require('./src/UserManager');
+let AccessTokenEvents = require('./src/AccessTokenEvents');
+let MetadataService = require('./src/MetadataService');
+let CordovaPopupNavigator = require('./src/CordovaPopupNavigator');
+let CordovaIFrameNavigator = require('./src/CordovaIFrameNavigator');
+let CheckSessionIFrame = require('./src/CheckSessionIFrame');
+let TokenRevocationClient = require('./src/TokenRevocationClient');
+let SessionMonitor = require('./src/SessionMonitor');
+let Global = require('./src/Global');
+let User = require('./src/User');
+
+export default {
+    Log,
+    OidcClient,
+    OidcClientSettings,
+    WebStorageStateStore,
+    InMemoryWebStorage,
+    UserManager,
+    AccessTokenEvents,
+    MetadataService,
+    CordovaPopupNavigator,
+    CordovaIFrameNavigator,
+    CheckSessionIFrame,
+    TokenRevocationClient,
+    SessionMonitor,
+    Global,
+    User
+};
